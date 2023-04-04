1ffbd069bc7355011f8ee452ad53c00d70723a16
mrgo1d <alejandroustin@gmail.com>
2023-04-04 16:51:38 +0300

    add login_page sample

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,22 @@
 from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
+link = "http://selenium1py.pythonanywhere.com/"
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page =  MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_login_form_are_exist(browser):
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
